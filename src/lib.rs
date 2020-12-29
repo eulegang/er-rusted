fedec875c61762c0a3a3c2861eb5f39aace02174
eulegang <eulegang@eulegang.dev>
2020-12-29 02:01:10 -0600

    Exposing datatypes as public

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,13 @@ mod resolve;
 mod syspoint;
 pub mod ui;
 
+pub use addr::{Address, Offset, Point};
 pub use buffer::Buffer;
+pub use cmd::{Command, SubstFlags};
 pub use interp::Interpreter;
+pub use re::{Expansion, Pat, Re};
+pub use syspoint::SysPoint;
+
 pub(crate) use parse::Parsable;
 
 pub(crate) const VALID_MARKS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ<>_";
