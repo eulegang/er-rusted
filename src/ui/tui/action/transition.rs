3df66ed9ee9c682f72415f56d8430e445bb25c1e
eulegang <eulegang@eulegang.dev>
2021-01-15 16:12:32 -0600

    Fixing append crash

--- src/ui/tui/action/transition.rs
+++ src/ui/tui/action/transition.rs
@@ -1,5 +1,6 @@
 use super::Action;
 use crate::ui::tui::{mode::Mode, Tui};
+use std::cmp::min;
 
 pub enum Transition {
     Insert,
@@ -12,9 +13,9 @@ impl Action for Transition {
     fn invoke(&self, tui: &mut Tui) -> eyre::Result<()> {
         match self {
             Transition::Insert => {}
-            Transition::Append => tui.cursor += 1,
+            Transition::Append => tui.cursor = min(tui.cmd.len(), tui.cursor + 1),
             Transition::HardInsert => tui.cursor = 0,
-            Transition::HardAppend => tui.cursor = tui.cmd.len() - 1,
+            Transition::HardAppend => tui.cursor = tui.cmd.len().checked_sub(1).unwrap_or(0),
         }
 
         tui.mode = Mode::Cmd;
